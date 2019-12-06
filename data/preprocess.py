711a70556f23c9da4203b5bd4a9b2d78d57c682e
Kinglog <391275002@qq.com>
2019-12-06 12:39:43 +0800

    模式串工作已完成，开展数据标定工作

--- data/preprocess.py
+++ data/preprocess.py
@@ -6,15 +6,19 @@
 import codecs
 import numpy as np
 import pandas as pd
+import os,sys
+import wave
 
-per_path = "个人菜单.txt"
-ent_path = "企业菜单.txt"
+per_path = "个人扩展.txt"
+ent_path = "企业扩展.txt"
 
-def getlist(path):
+def getlist(path, mode='caidan'):
     data = codecs.open(path, encoding="utf-8").readlines()
     result = []
     for line in data:
         line = line.strip()
+        if (mode == 'kuozhan'):
+            line = line.split('=')[0]
         result.append(line)
     return result
 
@@ -60,11 +64,64 @@ def output_similarity(data, pth, th=0.25):
         content += " ".join(["%s" % (k) for k in line]) + "\n\n"
         f.write(content)
 
-per_list = getlist(per_path)
-per_corr = calc_distance(per_list)
-output_similarity(per_corr, "per_corr.txt")
+def output_list(data,pth):
+    f = codecs.open(pth, "w", encoding="utf-8")
+    for i,line in enumerate(data):
+        f.write(line+'\t'+ str(i) +'\n')
 
-ent_list = getlist(ent_path)
-ent_corr = calc_distance(ent_list)
-output_similarity(ent_corr, "ent_corr.txt")
+# per_list = getlist(per_path,mode='caidan')
+# output_list(per_list, 'per_label.txt')
 
+# per_corr = calc_distance(per_list)
+# output_similarity(per_corr, "per_corr.txt")
+#
+# ent_list = getlist(ent_path)
+# output_list(ent_list, 'ent_label.txt')
+# ent_corr = calc_distance(ent_list)
+# output_similarity(ent_corr, "ent_corr.txt")
+
+
+def time_count(paths):
+    timecount = 0
+    validd = 0
+    sample_count = 0
+    error_file = []
+    for path in paths:
+        file_list = os.listdir(path)
+        for fi in file_list:
+            if ('.wav' in fi):
+                wav_path = path + fi
+                #print (wav_path)
+                try:
+                    with wave.open(wav_path, 'rb') as f:
+                        sample_count += 1
+                        timecount += f.getparams().nframes / f.getparams().framerate
+                        if (f.getparams().framerate==16000):
+                            validd += 1
+                        else:
+                            print(f.getparams().framerate)
+                            print (wav_path)
+                except wave.Error:
+                    error_file.append(wav_path)
+    return timecount,error_file,sample_count,validd
+path = ['/Users/kinglog/Documents/learn/computer/研究生/融港语音识别/labled/jsnx20191111/','/Users/kinglog/Documents/learn/computer/研究生/融港语音识别/labled/jy20191111/','/Users/kinglog/Documents/learn/computer/研究生/融港语音识别/labled/rg20191111/']
+
+# file_list = os.listdir(path[0])
+# wav_path = path[0] + file_list[0]
+# time_count = 0
+# with wave.open('/Users/kinglog/Documents/learn/computer/研究生/融港语音识别/labled/jy20191111/201901031408573124296.wav','rb') as f:
+#     print (f.getparams().nframes)
+#     print (f.getparams().framerate)
+#     time_count += f.getparams().nframes / f.getparams().framerate
+
+timecount,error_file,sample_count,validd = time_count(path)
+
+print (len(error_file))
+print ("sample_count", sample_count)
+print ("time len(s)", timecount)
+print (validd)
+
+fi = open('error_file.txt','w')
+for item in error_file:
+    item = item[len('/Users/kinglog/Documents/learn/computer/研究生/融港语音识别/'):]
+    fi.write(item+'\n')
